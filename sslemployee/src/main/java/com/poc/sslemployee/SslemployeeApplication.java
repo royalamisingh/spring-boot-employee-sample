7789464935bc96a8c363d84d41ff0f8f9445a7a7
royalamisingh <33050024+royalamisingh@users.noreply.github.com>
2019-04-10 14:17:55 +0530

    Employee wit SSl Config

---
+++ sslemployee/src/main/java/com/poc/sslemployee/SslemployeeApplication.java
@@ -0,0 +1,13 @@
+package com.poc.sslemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SslemployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SslemployeeApplication.class, args);
+	}
+
+}
